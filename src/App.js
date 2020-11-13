112d9d6d6f73aeb5a930b6e066b12f068e8ac65b
Brenda Long <brenda@studio6thave.com>
2020-11-13 16:16:29 -0600

    added bootstrap theme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
+import Button from 'react-bootstrap/Button';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        <h2>Show me the Font</h2>
+        <Button variant="primary">Primary</Button>
+      
         <a
           className="App-link"
           href="https://reactjs.org"
