963563831ce46198b364e04dfc50bc8198718945
Brenda Long <brenda@studio6thave.com>
2020-12-11 08:36:02 -0600

    fb stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import { ChrisList } from './components/journal/ChrisList'
+import React from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
+// import './App.css';
+// import Header from "./components/Header";
+import ApplicationViews from "./components/ApplicationViews";
+// import { ChrisList } from './components/journal/ChrisList';
+// import firebase from "firebase";
+// import { firebaseConfig } from "./components/fbAuth/firebaseConfig";
+import { Header } from "./components/Header";
+import { FirebaseProvider } from "./components/fbAuth/FirebaseProvider";
 
 
 function App() {
+
   return (
     <div className="App">
       <h1>All I want for Christmas...</h1>
-      <ChrisList />
+      <Router>
+        <FirebaseProvider>
+          <Header />
+          <ApplicationViews />
+        </FirebaseProvider>
+      </Router>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
-
 import './custom.scss';
+import { firebaseConfig } from "./components/fbAuth/firebaseConfig";
+import * as firebase from "firebase/app";
 
-
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_API_KEY,
+// };
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
